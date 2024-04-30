30431b8ad34830121659c94bcd3a8575fbdbee79
Christer Kaitila <breakdance@mcfunkypants.com>
2024-04-30 09:39:33 -0700

    minor tweak to getRndInteger to ensure max is returned as often as min

--- js/utilities/math.js
+++ js/utilities/math.js
@@ -1,4 +1,9 @@
 function getRndInteger(min, max) {
-    var Number = Math.random() * (max - min) + min;
-    return Math.floor(Number);
-  }
+	// this solution has a perfectly even distribution and is INCLUSIVE
+	// (can return max as often as any other number in the range). 
+	// read more:
+	// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+	min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
