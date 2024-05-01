b68687d9a03512538dd50ed44b807021e5758911
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2024-04-30 23:00:30 -0400

    Recreate mainOptions using uiFramework

--- js/uiFramework/uiContainer.js
+++ js/uiFramework/uiContainer.js
@@ -31,6 +31,8 @@ function UIView(x, y, width, height) {
 
 	this.backgroundColor = null;
 	this.isHighlighted = null;
+	this.highlightColor = null;
+	this.userInteractionEnabled = true;
 	this.subviews = [];
 
 	this.addSubView = function(view) {
@@ -64,8 +66,8 @@ function UIView(x, y, width, height) {
 		canvasContext.save();
 	  canvasContext.translate(this.x, this.y);
 
-		if (this.isHighlighted) {
-			colorRect(0, 0, this.width, this.height, 'yellow');
+		if (this.isHighlighted && this.highlightColor) {
+			colorRect(0, 0, this.width, this.height, this.highlightColor);
 		} else if (this.backgroundColor) {
 			colorRect(0, 0, this.width, this.height, this.backgroundColor);
 		}
@@ -82,14 +84,69 @@ function UIView(x, y, width, height) {
 	this.drawCustomContent = function() {}
 }
 
-const exampleMenuThing = new UIView(10, 10, 300, 100);
+// TODO: put UIComboButton class in its own file
+function UIComboButton(x, y, width, height) {
+	UIView.call(this, x, y, width, height);
+	this.image = null;
+	this.title = null;
+
+	this.textOffsetX = 5;
+
+	this.drawCustomContent = function() {
+		if (this.image) {
+			canvasContext.drawImage(this.image, 0, 0);
+		}
+		
+		if (this.title) {
+			const textColor = this.isHighlighted ? 'lime' : 'red';
+			colorText(this.title, this.textOffsetX, 65, textColor, "14px Arial Black");
+		}
+	}
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor
+UIComboButton.prototype = Object.create(UIView.prototype, {
+  constructor: {
+    value: UIComboButton,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  }
+});
+
+const exampleMenuThing = new UIView(10, 10, 275, 125);
 exampleMenuThing.backgroundColor = 'purple';
 addView(exampleMenuThing);
 
 // TODO: create actual UILabel class
-const exampleLabel = new UIView(5, 5, 150, 30);
+const exampleLabel = new UIView(15, 5, 150, 30);
 exampleLabel.drawCustomContent = function() {
-	colorText("New UI Thingy", 15, 20, "white", "14px Arial Black");
+	colorText("Turn Options", 0, 20, "white", "14px Arial Black");
 }
-exampleLabel.backgroundColor = 'green';
+// exampleLabel.backgroundColor = 'green';
 exampleMenuThing.addSubView(exampleLabel);
+
+const useItemButton = new UIComboButton(15, 40, 50, 75);
+// useItemButton.backgroundColor = 'white';
+useItemButton.image = useItemPic;
+useItemButton.title = "Items";
+exampleMenuThing.addSubView(useItemButton);
+
+const moveButton = new UIComboButton(80, 40, 50, 75);
+// moveButton.backgroundColor = 'cyan';
+moveButton.image = wizardMovementPic;
+moveButton.title = "Move";
+exampleMenuThing.addSubView(moveButton);
+
+const spellBoxButton = new UIComboButton(145, 40, 50, 75);
+// spellBoxButton.backgroundColor = 'pink';
+spellBoxButton.image = wizardSpellPic;
+spellBoxButton.title = "Spell";
+exampleMenuThing.addSubView(spellBoxButton);
+
+const endTurnButton = new UIComboButton(210, 40, 50, 75);
+// endTurnButton.backgroundColor = 'gray';
+endTurnButton.image = endTurnPic;
+endTurnButton.title = "End Turn";
+endTurnButton.textOffsetX = -10;
+exampleMenuThing.addSubView(endTurnButton);
