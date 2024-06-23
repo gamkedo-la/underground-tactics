aad1a21092fda75e9306ab32fb3e9d9310bfe2b9
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-06-23 12:22:55 -0500

    giving the textbox a null image on start

--- js/uiFramework/uiTest.js
+++ js/uiFramework/uiTest.js
@@ -3,7 +3,7 @@ let textBoxUI;
 function testUISetup() {
   textBoxUI = new UITextBox(10, 440, 400, 150);
   textBoxUI.text = "Welcome to UNDERGROUND TACTICS.\n\nClick an action (such as Move), then use\nthe arrow keys and spacebar to select\na target. Click End Turn to advance.\n\nNow go explore the underground!";
-  textBoxUI.image = endTurnPic;
+  textBoxUI.image = null;
   textBoxUI.backgroundImage = messagelogBackgroundPic;
   addView(textBoxUI);
 }
